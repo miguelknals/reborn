6a144b969ca45b5cd00b395e9bb958fd7f313f0f
m canals <infomknals@gmail.com>
2019-01-04 15:37:44 +0100

    Add project files.

---
+++ task01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Task 00");
+        }
+    }
+}
